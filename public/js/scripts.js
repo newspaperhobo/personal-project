f66a4a7222e5476b4d1a31a82263c7dc3f3f6916
newspaperhobo <hoboinabox@gmail.com>
2021-04-21 23:10:35 -0400

    initial commit

---
+++ public/js/scripts.js
@@ -0,0 +1,72 @@
+// let showAll = document.querySelector("#showall")
+// ;
+// let desc = document.querySelectorAll(".hide");
+
+// const showAllDesc = function(e) {
+    //     for (let i=0; i< desc.length; i++) {
+        //         desc[i].classList.toggle("show");
+        //         e.stopPropagation()
+        // }}
+        
+        // showAll.addEventListener("click", showAllDesc);
+
+        
+        // mobile nav
+        let navIcon = document.querySelector("i");
+        let navList = document.querySelector(".navList");
+        
+        const showHideNav = function() {
+            navList.classList.toggle("navList")
+        }
+        
+        navIcon.addEventListener("click", showHideNav);
+        
+        // library.html accordian
+        // let img = document.querySelectorAll(".individual-card");
+        // let carrot = document.querySelector(".fa-chevron-down")
+
+        
+        // const showHide = function() {
+        //     for (let i = 0; i < img.length; i++)
+        //     img[i].classList.toggle("show")
+        // }
+        
+        // carrot.addEventListener("click", showHide);
+
+        let body = document.querySelector("body"); 
+        let icons = document.querySelectorAll("i");
+        // add hover black
+        let notebook = document.querySelector(".notebook");
+        let nav = document.querySelector(".nav");
+        let filter = document.querySelector(".library-sort");
+        let theme = document.querySelector(".theme")
+        let themes = document.querySelectorAll(".theme")
+        const theme2 = function() {
+            if (body.style.backgroundColor = "var(--secondary-background-color)") {
+            body.style.backgroundColor = "var(--secondary-background-color)"
+            for (let i=0; i < icons.length; i++) {
+            icons[i].style.color = "black";
+            }
+            notebook.style.backgroundImage = "url('../public/imgs/backgrounds/amanita3.png')";
+            nav.style.backgroundColor = "red";
+            filter.style.backgroundColor = "red";
+            for (let i=0; i < themes.length; i++) {
+                themes[i].style.backgroundColor = "var(--secondary-background-color)"
+                }
+            } else theme1()
+        }
+
+        
+        theme.addEventListener("click", theme2);
+        
+        // const theme1 = function() {
+        //     body.style.backgroundColor = "var(--primary-background-color)";
+        //     for (let i=0; i < icons.length; i++) {
+        //     icons[i].style.color = "var(--primary-nav-font-color)";
+        //     }
+        //     notebook.style.backgroundImage = "none"
+        //     nav.style.backgroundColor = "var(--primary-nav-background)";
+        //     filter.style.backgroundColor = "var(----primary-nav-background)";
+        //     theme.style.backgroundColor = "var(---primary-button-yellow)"
+        // }
+        
