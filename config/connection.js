9f4eaad698a31aba921b7f8a13abb75f35c1c75c
newspaperhobo <hoboinabox@gmail.com>
2021-06-10 17:43:57 -0400

    adding loop in list view, fixing styling of library page, set usefindandmodify to false in connection

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
-    useFindAndModify: true, 
+    useFindAndModify: false, 
     useCreateIndex: true 
 }, error => {
     if (error) {
         console.log('Error connecting to MongoDB server.')
+        console.log(error)
     } else {
         console.log('Connection to MongoDB successful.')
     }
