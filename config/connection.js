1bd2dac94361d3a7e46c0d58ec647628ed5e4f2c
newspaperhobo <hoboinabox@gmail.com>
2023-03-24 01:20:01 -0400

    return db connection to original state

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.DB_URL, {
-//     useNewUrlParser: true, 
-//     useUnifiedTopology: true, 
-//     useFindAndModify: false, 
-//     useCreateIndex: true 
-// }, error => {
-//     if (error) {
-//         console.log('Error connecting to MongoDB server.')
-//         console.log(error)
-//     } else {
-//         console.log('Connection to MongoDB successful.')
-//     }
-// })
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useFindAndModify: false, 
+    useCreateIndex: true 
+}, error => {
+    if (error) {
+        console.log('Error connecting to MongoDB server.')
+        console.log(error)
+    } else {
+        console.log('Connection to MongoDB successful.')
+    }
+})
 
-try {
-  await mongoose.connect(process.env.DB_URL);
-} catch (error) {
-  handleError(error);
-}
+// try {
+//   await mongoose.connect(process.env.DB_URL);
+// } catch (error) {
+//   handleError(error);
+// }
